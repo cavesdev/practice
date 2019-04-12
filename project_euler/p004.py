58f85b229f440ea2d409daaa0e84f7ce697b582d
cavesdev <24899540+cavesdev@users.noreply.github.com>
2019-04-12 16:35:10 -0500

    corrected, now gives correct answer

--- project_euler/p004.py
+++ project_euler/p004.py
@@ -6,9 +6,9 @@ Problem 4 from Project Euler
 Carlos Cuevas
 11-04-19
 """
-from math import ceil
 
 # functions
+
 
 def divide_in_digits(number):
     """Returns a list with every digit from the input number"""
@@ -21,16 +21,19 @@ def divide_in_digits(number):
 
     return digits
 
+
 def compare_lists(first, second):
     """Compares two lists object by object"""
-    for i in range(0, len(first)):
-        if first[i] != second[i]:
+    for item in range(0, len(first)):
+        if first[item] != second[item]:
             return False
-    else: return True
+    else:
+        return True
 
 
 # main program
 
+answer = [0, 0, 0]
 # test every three digit number
 for i in range(100, 1000):
     for j in range(100, 1000):
@@ -38,6 +41,7 @@ for i in range(100, 1000):
         # reverse list
         digits_inv = digits[::-1]
         if compare_lists(digits, digits_inv) is True:
-            answer = [i, j, i * j]
+            if answer[2] < i * j:
+                answer = [i, j, i * j]
 
 print(answer)
