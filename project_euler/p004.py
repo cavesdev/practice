2add1657393c76831bed3d668c6c81a59c2b2fa6
cavesdev <24899540+cavesdev@users.noreply.github.com>
2019-04-11 19:19:37 -0500

    work done, incorrect

--- project_euler/p004.py
+++ project_euler/p004.py
@@ -6,4 +6,50 @@ Problem 4 from Project Euler
 Carlos Cuevas
 11-04-19
 """
+from math import ceil
 
+# functions
+
+def divide_in_digits(number):
+    """Returns a list with every digit from the input number"""
+    digits = []
+
+    # fill the list with digits
+    while number != 0:
+        digits.insert(0, number % 10)
+        number = number // 10
+
+    return digits
+
+def divide_list(digits):
+    """Divides a list in two sublists: one with original left half and the other one inverted"""
+    left = digits[:len(digits) // 2]
+    right = digits[ceil(len(digits) // 2):]
+    right.reverse()
+
+    return (left, right)
+
+def compare_lists(first, second):
+    """Compares two lists object by object"""
+    for i in range(0, len(first)):
+        if first[i] != second[i]:
+            return False
+    else: return True
+
+
+# main program
+
+# test every three digit number
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        digits = divide_in_digits(i * j)
+        # skip if length is not even
+        if len(digits) % 2 != 0:
+            continue
+            print("not equal!")
+        digits = divide_list(digits)
+        #print(digits)
+        if compare_lists(digits[0], digits[1]) is True:
+            answer = i * j
+
+print(answer)
